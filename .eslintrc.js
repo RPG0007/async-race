b8296c54753aa29f3571041715ae21c452822e91
RPG0007 <90514460+RPG0007@users.noreply.github.com>
2023-01-19 17:28:54 +0300

    init:add base config and car component

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+  plugins: ['@typescript-eslint', 'import'],
+  extends: [
+    'eslint:recommended',
+    'airbnb-typescript/base',
+    'plugin:@typescript-eslint/recommended',
+    // Uses the recommended rules from @typescript-eslint/eslint-plugin
+  ],
+
+  parserOptions: {
+    ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
+    sourceType: 'module', // Allows for the use of imports
+    ecmaFeatures: {
+      jsx: true, // Allows for the parsing of JSX
+    },
+    project: './tsconfig.json',
+  },
+  rules: {
+    // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
+    // e.g. '@typescript-eslint/explicit-function-return-type': 'off',
+  },
+};
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import './styles.scss';
+import { carIcon } from './components/car/index';
+
+window.addEventListener('DOMContentLoaded', () => {});
